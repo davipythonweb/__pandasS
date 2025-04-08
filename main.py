500fe85a4d106644758cd4cf1f2ce5cfff23105c
davipythonweb <galaxys3tab32@gmail.com>
2025-04-08 00:21:50 -0300

    upload de datasets e class1

--- main.py
+++ main.py
@@ -3,3 +3,22 @@
 a = 'Analise_de_Dados'
 
 print('ola mundo!',a)
+
+"""-------------------------"""
+import pandas as pd
+
+print("versao do pandas =",pd.__version__)
+
+# biblioteca de datasets
+import kagglehub
+
+# Download latest version ( baixando DATASETS da alura)
+path = kagglehub.dataset_download("murilomorgado/dados-alura-python-para-data-science")
+
+# mostrando o link para download
+print("Path to dataset files:", path)
+
+
+# exibindo os dados do dataset-telecon
+dados_churn = pd.read_json("dataset-telecon.json")
+print(dados_churn)
